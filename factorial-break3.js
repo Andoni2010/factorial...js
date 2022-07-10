f401ddd84bb3a6b00d414ee16e6006f83a192a23
andoni <98291437+Andoni2010@users.noreply.github.com>
2022-07-10 11:16:25 +0200

    Add files via upload

---
+++ factorial-break3.js
@@ -0,0 +1,14 @@
+// Bucles con el break
+
+let CorteAprobado = 4;
+
+switch (CorteAprobado) {
+    case 5:
+        console.log("Aprobado")
+        break;
+    case 4:
+        console.log("Suspendido")
+        break;
+    default:
+        break;
+}
---
+++ factorial-while3.js
@@ -0,0 +1,9 @@
+// Bucle con while
+
+
+let min = 1;
+let max = 10;
+while ( min <= max){
+    console.log(min);
+    min++;
+}
